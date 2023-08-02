d44e93f620249a86bf31d2dc4b04da4f1e1d19a8
tunan <tunan@gaoding.com>
2023-08-02 15:45:45 +0800

    feat: add reset event

--- src/module/request/Counter.ts
+++ src/module/request/Counter.ts
@@ -1,3 +1,5 @@
+import { ECounter } from "../../core/trigger/event/ECounter";
+import { Trigger } from "../../core/trigger/function/Trigger";
 import { Module } from "../base/Module";
 
 // RequestCounter类用于计算请求的频率和次数
@@ -41,13 +43,24 @@ export class RequestCounter extends Module {
         this._count = count;
     }
 
+    // 触发器实例，用于触发事件
+    private _trigger = new Trigger<ECounter>();
+
+    // 获取触发器实例
+    get trigger() {
+        return this._trigger;
+    }
+
     // 构造函数，接受一个可选的时间窗口参数，单位为秒
     constructor(interval?: number) {
         super();
 
-        // 如果传入了时间窗口参数，则将时间间隔转换为毫秒并赋值给_interval
-        if(interval)
+        // 如果传入了时间窗口参数，则将时间窗口转换为毫秒并赋值给_interval
+        if (interval)
             this.interval = interval * 1000;
+
+        // 注册触发器的'reset'事件，当触发该事件时执行reset方法
+        this.trigger.once('reset', () => this.reset());
     }
 
     // addRequest方法用于增加请求计数
@@ -56,12 +69,17 @@ export class RequestCounter extends Module {
         const now = Date.now();
 
         // 如果当前时间与上次计数重置的时间间隔超过了设定的时间窗口，则将计数器重置为0，并更新上次计数重置的时间戳
-        if(now - this.lastReset > this.interval) {
-            this.count = 0;
-            this.lastReset = now;
-        }
+        if (now - this.lastReset > this.interval)
+            this.reset();
 
         // 计数器加1
         this.count++;
+    }
+
+    // reset方法用于重置计数器
+    reset(now?: number) {
+        // 将计数器重置为0，并更新上次计数重置的时间戳
+        this.count = 0;
+        this.lastReset = now || Date.now();
     }
 }
